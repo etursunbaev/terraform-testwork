85159a5ce052dcad303cafe9be4629f2cf7d70f4
U-MOSCOW\Eldar_Tursunbaev <Eldar_Tursunbaev@EPKZALMW0245.moscow.epam.com>
2022-04-21 00:28:32 +0600

    ASG module initial commit.

---
+++ task5/modules/asg-tfm/main.tf
@@ -0,0 +1,6 @@
+resource "aws_autoscaling_group" "AiAl" {
+  name                 = "${var.prefix_name}-${var.environment}-asg"
+  max_size             = var.asg_max_size
+  min_size             = var.asg_min_size
+  launch_configuration = var.launch_config_name
+}
---
+++ task5/modules/asg-tfm/terraform.tfvars
@@ -0,0 +1,5 @@
+prefix_name        = "moi"
+environment        = "test"
+asg_max_size       = 2
+asg_min_size       = 1
+launch_config_name = "moi-test-lcfg"
