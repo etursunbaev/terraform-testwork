d93cffdd012ef693fafd7f823b55b8d010ad0fd6
U-MOSCOW\Eldar_Tursunbaev <Eldar_Tursunbaev@EPKZALMW0245.moscow.epam.com>
2022-04-22 16:01:49 +0600

    Removed launch-conf module

--- task5/modules/launch-config-tfm/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "launch_configuration_name" {
-  description = "The Launch Configuration name."
-  value       = aws_launch_configuration.asg_launch_conf.name
-}
--- task5/modules/launch-config-tfm/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-prefix_name  = "my"
-environment  = "test"
-pub_key_name = "MyKeyPair"
-image_id = "ami-03ededff12e34e59e"
