fd385a1f6b258323f7af8ebb44b800dd0ed1166f
U-MOSCOW\Eldar_Tursunbaev <Eldar_Tursunbaev@EPKZALMW0245.moscow.epam.com>
2022-04-22 21:03:34 +0600

    Formatted and added condition

--- task5/modules/ecs-tfm/main.tf
+++ task5/modules/ecs-tfm/main.tf
@@ -1,12 +1,12 @@
 resource "aws_ecs_cluster" "cluster" {
-    name = var.cluster_name != "" && var.create_ecs ? var.cluster_name : "example"
+  name = var.cluster_name != "" && var.create_ecs ? var.cluster_name : "example"
 }
 resource "aws_iam_role" "ecsInstanceRole" {
-  name               = "${var.prefix_name}-${var.environment}-ecsInstanceRole" : 
+  name               = var.prefix_name != "" && var.environment != "" ? "${var.prefix_name}-${var.environment}-ecsInstanceRole" : "default-ecsInstanceRole"
   assume_role_policy = var.ecsInstanceRoleAssumeRolePolicy
 }
 resource "aws_iam_role_policy" "ecsInstanceRolePolicy" {
-  name   = "${var.prefix_name}-${var.environment}-ecsInstanceRolePolicy"
+  name   = var.prefix_name != "" && var.environment != "" ? "${var.prefix_name}-${var.environment}-ecsInstanceRolePolicy" : "default--ecsInstanceRolePolicy"
   role   = aws_iam_role.ecsInstanceRole.id
   policy = var.ecsInstancerolePolicy
 }
--- task5/modules/ecs-tfm/variables.tf
+++ task5/modules/ecs-tfm/variables.tf
@@ -9,9 +9,9 @@ variable "environment" {
   default     = "test"
 }
 variable "cluster_name" {
-    description = "Name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
-    type = string
-    default = ""
+  description = "Name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+  type        = string
+  default     = ""
 }
 variable "ecsInstanceRoleAssumeRolePolicy" {
   type = string
