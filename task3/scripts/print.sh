88f46cdf02ba6c3a0f7f441b66742cb51d124997
U-MOSCOW\Eldar_Tursunbaev <Eldar_Tursunbaev@EPKZALMW0245.moscow.epam.com>
2022-04-26 22:25:07 +0600

    Removed prev tasks

--- task3/scripts/print.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/cat /tmp/success.txt
--- task4/scripts/root-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo su
-
-# install httpd (Linux 2 version)
-yum update -y
-amazon-linux-extras install nginx1 -y
-echo "SUCCESS!" > /tmp/success.txt
-touch /var/tmp/finished-user-data
